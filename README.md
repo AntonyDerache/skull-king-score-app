73917e6dfb142b7f62e48e9f7cb968ab6b74d636
AntonyDerache <52654615+AntonyDerache@users.noreply.github.com>
2024-03-06 18:12:38 +0100

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# skull_king_score_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/skull_king_score_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skull_king_score_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
