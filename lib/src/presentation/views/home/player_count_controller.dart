9890a424713554656620e61a785cbf75bf5a94d0
AntonyDerache <52654615+AntonyDerache@users.noreply.github.com>
2024-06-21 16:09:26 +0200

    forgot to push one file from last commit

--- lib/src/presentation/views/home/player_count_controller.dart
+++ lib/src/presentation/views/home/player_count_controller.dart
@@ -3,15 +3,16 @@ import 'package:skull_king_score_app/src/presentation/widgets/sk_text.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class PlayerCountController extends StatelessWidget {
-  const PlayerCountController(
-      {super.key,
-      this.numberOfPlayers = 0,
-      required this.add,
-      required this.remove});
+  const PlayerCountController({
+    super.key,
+    this.numberOfPlayers = 0,
+    required this.add,
+    required this.remove,
+  });
 
   final int numberOfPlayers;
-  final Function(BuildContext) add;
-  final Function(BuildContext) remove;
+  final Function() add;
+  final Function() remove;
 
   @override
   Widget build(BuildContext context) {
@@ -19,21 +20,24 @@ class PlayerCountController extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(
-            icon: const Icon(Icons.remove),
-            iconSize: 32,
-            color: Colors.white,
-            disabledColor: Colors.grey,
-            onPressed: numberOfPlayers <= 2 ? null : () => remove(context)),
+          icon: const Icon(Icons.remove),
+          iconSize: 32,
+          color: Colors.white,
+          disabledColor: Colors.grey,
+          onPressed: numberOfPlayers <= 2 ? null : remove,
+        ),
         SKText(
-            text:
-                '$numberOfPlayers / 8 ${AppLocalizations.of(context)!.players}',
-            color: Colors.white),
+          text: '$numberOfPlayers / 8 ${AppLocalizations.of(context)!.players}',
+          color: Colors.white,
+        ),
         IconButton(
-            icon: const Icon(Icons.add),
-            iconSize: 32,
-            color: Colors.white,
-            disabledColor: Colors.grey,
-            onPressed: numberOfPlayers >= 8 ? null : () => add(context)),
+          key: const ValueKey('btn_add_player'),
+          icon: const Icon(Icons.add),
+          iconSize: 32,
+          color: Colors.white,
+          disabledColor: Colors.grey,
+          onPressed: numberOfPlayers >= 8 ? null : add,
+        ),
       ],
     );
   }
